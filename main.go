58a5a559993ab24eb881605ac58321ee016262b4
svwielga4 <svwielga4@gmail.com>
2019-10-31 11:11:52 -0700

    playing with flags package

--- main.go
+++ main.go
@@ -2,19 +2,28 @@ package main
 
 import (
 	"encoding/csv"
+	"flag"
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 func main() {
-	file, err := os.Open(os.Args[1])
+	// Define flags with defaults
+	f := flag.String("file", "problems.csv", "the csv with quiz problems - problems.csv")
+	d := flag.Duration("time", time.Second*5, "How long should the quiz be - 5 seconds")
+	// Load flag values
+	flag.Parse()
+
+	// Open the file
+	file, err := os.Open(*f)
 	if err != nil {
 		panic(err)
 	}
 
+	// Create a reader and read from the csv
 	cr := csv.NewReader(file)
-
 	for {
 		r, err := cr.Read()
 		if err == io.EOF {
