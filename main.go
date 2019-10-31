d5f0f778471d0b44091e0d06651fb4c6a84dd3d3
svwielga4 <svwielga4@gmail.com>
2019-10-31 12:16:38 -0700

    added a problem struct and a helper exit func

--- main.go
+++ main.go
@@ -4,36 +4,37 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
-	"io"
 	"os"
-	"time"
 )
 
 func main() {
 	// Define flags with defaults
-	f := flag.String("file", "problems.csv", "the csv with quiz problems - problems.csv")
-	d := flag.Duration("time", time.Second*5, "How long should the quiz be - 5 seconds")
+	f := flag.String("csv", "problems.csv", "the csv with quiz problems - problems.csv")
+	// d := flag.Duration("time", time.Second*5, "How long should the quiz be - 5 seconds")
 	// Load flag values
 	flag.Parse()
 
 	// Open the file
 	file, err := os.Open(*f)
 	if err != nil {
-		panic(err)
+		exit(fmt.Sprintf("Failed to open the CSV: %s\n", *f))
 	}
 
 	// Create a reader and read from the csv
 	cr := csv.NewReader(file)
-	for {
-		r, err := cr.Read()
-		if err == io.EOF {
-			break
-		}
-
-		if err != nil {
-			panic(err)
-		}
-
-		fmt.Println(r)
+	lines, err := cr.ReadAll()
+	if err != nil {
+		exit("Failed to parse CSV")
 	}
+	fmt.Println(lines)
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
 }
